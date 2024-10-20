568d56774a698698f8f36852b947d305b8608c82
BayanADA <Bayanalshuwaier@gmail.com>
2024-10-20 12:43:24 +0300

    Initial Commit

---
+++ Recipe App/Recipe_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_AppApp.swift
+//  Recipe App
+//
+//  Created by Bayan Alshuwaier on 20/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Recipe AppTests/Recipe_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_AppTests.swift
+//  Recipe AppTests
+//
+//  Created by Bayan Alshuwaier on 20/10/2024.
+//
+
+import Testing
+@testable import Recipe_App
+
+struct Recipe_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

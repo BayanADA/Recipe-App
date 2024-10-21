b41bbeaaa649e6ac6674b4c10c0510a20b61ba83
BayanADA <Bayanalshuwaier@gmail.com>
2024-10-21 13:58:55 +0300

    6

--- Recipe App/RecipePage.swift
+++ Recipe App/RecipePage.swift
@@ -19,97 +19,90 @@ struct RecipePage: View {
         UINavigationBar.appearance().standardAppearance = appearance
         UINavigationBar.appearance().scrollEdgeAppearance = appearance
     }
-    var body: some View {
-NavigationStack{
-
-
-ZStack{
-    Rectangle()
-        .fill(Color.mainGray)
-        .strokeBorder(style: StrokeStyle(lineWidth: 2, dash: [5]))
-        .foregroundColor(.mainOrange)
-        .frame(width: 413, height: 181)
-  
-    
-    VStack{
+var body: some View {
+    NavigationStack{
         
-        Image(systemName: "photo.badge.plus")
-            .foregroundColor(.mainOrange)
-            .font(.system(size: 60))
-        
-        
-        Text("Upload Photo")
-            .font(.system(size: 22))
-            .bold()
-        
-        
-    }
-    
-    
-}
-
-
-
-
-VStack{
-    Text("Title")
-        .font(.system(size: 24))
-        .bold()
-        .frame(width: 370.5, height: 29, alignment: .leading)
-    
-    TextField("Title", text: $titleText)
-        .font(.system(size: 24))
-        .padding()
-        .frame(width: 370.5)
-        .background(Color.mainGray)
-        .cornerRadius(8)
-        .multilineTextAlignment(.leading)
-
-
-    
-        
-    
-    Text("Description")
-        .font(.system(size: 24))
-        .bold()
-        .frame(width: 370.5, height: 29, alignment: .leading)
-    
-    TextField("Description", text: $descriptionText, axis: .vertical)
-        .font(.system(size: 24))
-        .padding()
-        .frame(width: 370.5)
-        .background(Color.mainGray)
-        .cornerRadius(8)
-        .multilineTextAlignment(.leading)
-        .lineLimit(4...10)
-    
-    HStack{
-        Text("Add Ingrediant")
-            .font(.system(size: 24))
-            .bold()
-         Spacer()
-        Text(.init(systemName: "plus"))
-            .foregroundColor(.mainOrange)
-        
+        VStack{
             
-    }
-    .frame(width: 370.5, height: 29)
-    
-}
-    
-    
-
-    
-        .navigationTitle("New Recipe")
-        .toolbar{
-            ToolbarItem{
-                Text("Save")
+            ZStack{
+                
+                Rectangle()
+                    .fill(Color.mainGray)
+                    .strokeBorder(style: StrokeStyle(lineWidth: 2, dash: [5]))
                     .foregroundColor(.mainOrange)
+                    .frame(width: 413, height: 181)
+                
+                
+                VStack{
+                    
+                    Image(systemName: "photo.badge.plus")
+                        .foregroundColor(.mainOrange)
+                        .font(.system(size: 60))
+                    
+                    
+                    Text("Upload Photo")
+                        .font(.system(size: 22))
+                        .bold()
+                }
+            }
+            VStack{
+                Text("Title")
+                    .font(.system(size: 24))
+                    .bold()
+                    .frame(width: 370.5, height: 29, alignment: .leading)
+                
+                TextField("Title", text: $titleText)
+                    .font(.system(size: 24))
+                    .padding()
+                    .frame(width: 370.5)
+                    .background(Color.mainGray)
+                    .cornerRadius(8)
+                    .multilineTextAlignment(.leading)
+                
+                
+                
+                
+                
+                Text("Description")
+                    .font(.system(size: 24))
+                    .bold()
+                    .frame(width: 370.5, height: 29, alignment: .leading)
+                
+                TextField("Description", text: $descriptionText, axis: .vertical)
+                    .font(.system(size: 24))
+                    .padding()
+                    .frame(width: 370.5)
+                    .background(Color.mainGray)
+                    .cornerRadius(8)
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(4...10)
+                
+                HStack{
+                    Text("Add Ingrediant")
+                        .font(.system(size: 24))
+                        .bold()
+                    Spacer()
+                    Text(.init(systemName: "plus"))
+                        .foregroundColor(.mainOrange)
+                    
+                    
+                }
+                .frame(width: 370.5, height: 29)
                 
             }
-            
-        }}}}
-
+           
+            .navigationTitle("New Recipe")
+            .toolbar{
+                ToolbarItem{
+                    Text("Save")
+                        .foregroundColor(.mainOrange)
+                }
+             }
+         }
+        .padding(.top, -90)
+     }
+ }
+}
 #Preview {
-    RecipePage()
+RecipePage()
 }

9b57619bb5e63ec4e58992ef801155c66af3ca35
BayanADA <Bayanalshuwaier@gmail.com>
2024-10-21 12:40:52 +0300

    4

--- Recipe App/RecipePage.swift
+++ Recipe App/RecipePage.swift
@@ -17,36 +17,86 @@ struct RecipePage: View {
         UINavigationBar.appearance().scrollEdgeAppearance = appearance
     }
     var body: some View {
-        NavigationStack{
-            VStack{
-                ZStack{
-                    Rectangle()
-                        .fill(Color.mainGray)
-                        .strokeBorder(style: StrokeStyle(lineWidth: 2, dash: [5]))
-                        .foregroundColor(.mainOrange)
-                        .frame(width: 413, height: 181)
-                    
-                    
-                    VStack{
-                        
-                        Image(systemName: "photo.badge.plus")
-                            .foregroundColor(Color("mainOrange"))
-                            .font(.system(size: 60))
-                        
-                        
-                        Text("Upload Photo")
-                            .font(.system(size: 22))
-                            .bold()
-                        
-                        
-                    }
-                    
-                }
+NavigationStack{
+
+
+ZStack{
+    Rectangle()
+        .fill(Color.mainGray)
+        .strokeBorder(style: StrokeStyle(lineWidth: 2, dash: [5]))
+        .foregroundColor(.mainOrange)
+        .frame(width: 413, height: 181)
+  
+    
+    VStack{
+        
+        Image(systemName: "photo.badge.plus")
+            .foregroundColor(.mainOrange)
+            .font(.system(size: 60))
+        
+        
+        Text("Upload Photo")
+            .font(.system(size: 22))
+            .bold()
+        
+        
+    }
+    
+    
+}
+
+
+
+
+VStack{
+    Text("Title")
+        .font(.system(size: 24))
+        .bold()
+        .frame(width: 366, height: 29, alignment: .leading)
+    
+    TextField("Title", text: .constant(""))
+        .font(.system(size: 24))
+        .padding()
+        .frame(width: 366, height: 47)
+        .background(Color.mainGray)
+        .cornerRadius(8)
+    
+    Text("Description")
+        .font(.system(size: 24))
+        .bold()
+        .frame(width: 370.5, height: 29, alignment: .leading)
+    
+    TextField("Description", text: .constant(""))
+        .font(.system(size: 24))
+        .padding()
+        .frame(width: 368, height: 130)
+        .background(Color.mainGray)
+        .cornerRadius(8)
+    HStack{
+        Text("Add Ingrediant")
+            .font(.system(size: 24))
+            .bold()
+         Spacer()
+        Text(.init(systemName: "plus"))
+            .foregroundColor(.mainOrange)
+            
+    }
+    .frame(width: 371.5, height: 29)
+    
+}
+    
+    
+
+    
+        .navigationTitle("Recipe Page")
+        .toolbar{
+            ToolbarItem{
+                Text("Save")
+                    .foregroundColor(.mainOrange)
                 
-                
-                    .navigationTitle("Recipe Page")
-                
-            }}}}
+            }
+            
+        }}}}
 
 #Preview {
     RecipePage()
